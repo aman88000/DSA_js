3396e22f9f7c49f222d0e8c95a7bc69bcf94cf94
aman88000 <aman88000@gmail.com>
2025-09-02 22:37:42 +0530

    find odd number using helper recursive function

--- scripts/oddNumRecursion.js
+++ scripts/oddNumRecursion.js
@@ -1,13 +1,33 @@
 const arr = [1,2,3,4,5,6,7,8,9,10];
-const oddArr = []
+// const oddArr = []
 
-function oddNum(arr){
-for(let i = 0; i <arr.length; i++){
-    if(arr[i]%2 !==0){
-        oddArr.push(arr[i])
+// function oddNum(arr){
+// for(let i = 0; i <arr.length; i++){
+//     if(arr[i]%2 !==0){
+//         oddArr.push(arr[i])
+//     }
+// }
+// return oddArr
+// }
+
+// console.log(oddNum(arr))
+
+
+
+function findOdd(arr){
+    let result = []
+
+    function helperRecursive(arr){
+        if(arr.length === 0){
+            return 
+        }
+        if(arr[0]%2!==0){
+            result.push(arr[0])
+        }
+        helperRecursive(arr.slice(1))
     }
-}
-return oddArr
+    helperRecursive(arr)
+    return result
 }
 
-console.log(oddNum(arr))
+console.log(findOdd(arr))
