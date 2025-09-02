48ea9c3fc8b12d1a362414c90bca874e58d51a7a
aman88000 <aman88000@gmail.com>
2025-09-02 21:57:06 +0530

    modifield sorting function no need to call isSort() in else if and else code

--- scripts/sortingArray.js
+++ scripts/sortingArray.js
@@ -1,36 +1,67 @@
 const arr = [1,4,5,6,9,0,2,10,-1,7,3, 8, 0];
 
 
+// function isSort(arr){
+// for(let i = 0; i <arr.length; i++){
+//     if(arr[i] > arr[i+1]){
+//     console.log(i)
+//      return false
+//     }
+// }
+// return true
+// }
+// //console.log(isSort(arr))
+
+// let i = 0;
+// let j = 1;
+// function sorting(arr){
+//     if(isSort(arr)){
+//         return arr
+//     } else if(arr[i] < arr[j]){
+//         i++
+//         j++
+//         sorting(arr)
+//         isSort(arr)
+//     }else{
+//         [arr[i], arr[j]] = [arr[j], arr[i]]
+//         i = 0;
+//         j = 1
+//         sorting(arr)
+//         isSort(arr)
+//     }
+//     return arr
+// }
+// // sorting(arr)
+// console.log(sorting(arr))
+
+
+
 function isSort(arr){
-for(let i = 0; i <arr.length; i++){
-    if(arr[i] > arr[i+1]){
-    console.log(i)
-     return false
+    for(let i = 0; i< arr.length; i++){
+        if(arr[i]> arr[i+1]){
+            return false
+        }
     }
+    return true
 }
-return true
-}
-//console.log(isSort(arr))
 
 let i = 0;
 let j = 1;
 function sorting(arr){
     if(isSort(arr)){
         return arr
-    } else if(arr[i] < arr[j]){
+    }else if(arr[i]<arr[j]){
         i++
         j++
-        sorting(arr)
-        isSort(arr)
+        sorting(arr);
+        // isSort(arr)
     }else{
-        [arr[i], arr[j]] = [arr[j], arr[i]]
+        [arr[i], arr[j]]= [arr[j], arr[i]]
         i = 0;
-        j = 1
+        j = 1;
         sorting(arr)
-        isSort(arr)
+        // isSort(arr)
     }
-    return arr
+  return arr
 }
-// sorting(arr)
 console.log(sorting(arr))
-
