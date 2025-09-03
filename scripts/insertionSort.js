ffb1fa92ba110d46349bf89ae29ea69c02c9b26c
aman88000 <aman88000@gmail.com>
2025-09-03 18:44:40 +0530

    insertion sort taking from 2nd number and than compare with left number

--- scripts/insertionSort.js
+++ scripts/insertionSort.js
@@ -1,24 +1,45 @@
 const arr = [1,4,7,5,9,2,6,3,8]
 
-function insertionSort(arr){
+// function insertionSort(arr){
+//     if(arr.length === 0){
+//         return false
+//     }
+//     for(let i=0; i<arr.length; i++){
+//         let min = i
+//         // let temp;
+
+//         for(let j=i+1; j<arr.length; j++){
+//             if(arr[j] < arr[i]){
+//                 min = j
+//             }
+//         }
+//         // temp = arr[i]
+//         // arr[i] = arr[min]
+//         // arr[min] = temp
+//         [arr[i], arr[min]]= [arr[min], arr[i]]
+//     }
+//     return arr
+// }
+
+// console.log(insertionSort(arr))
+
+
+
+
+function insertionSort2ndElement(arr){
     if(arr.length === 0){
         return false
     }
-    for(let i=0; i<arr.length; i++){
-        let min = i
-        // let temp;
-
-        for(let j=i+1; j<arr.length; j++){
-            if(arr[j] < arr[i]){
-                min = j
-            }
+    for(let i =1; i<arr.length; i++){
+        let curr = arr[i]
+        let j = i-1
+        while(j>=0 && arr[j]>curr){
+            arr[j+1] = arr[j]
+            j--
         }
-        // temp = arr[i]
-        // arr[i] = arr[min]
-        // arr[min] = temp
-        [arr[i], arr[min]]= [arr[min], arr[i]]
+        arr[j+1] = curr
     }
     return arr
 }
 
-console.log(insertionSort(arr))
+console.log(insertionSort2ndElement(arr))
